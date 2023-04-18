6ef8d38a915b6a13b5aa41ce806dfc6dbd55daff
Bhavitha <sbbhavitha2002@gmail.com>
2023-04-18 23:53:08 +0530

    ksdkzn

---
+++ grades.py
@@ -0,0 +1,13 @@
+marks = int(input('Enter the marks: '))
+if (marks>=90 and marks<=100) :
+    print('Grade-A')
+if (marks>=80 and marks<90) :
+    print('Grade-B')
+if (marks>=70 and marks<80) :
+    print('Grade-C')
+if (marks>=60 and marks<=70) :
+    print('Grade-D')
+if (marks>=50 and marks<=60) :
+    print('Grade-E')
+else:
+    print('Grade-F')
